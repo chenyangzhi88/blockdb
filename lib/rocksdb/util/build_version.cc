0c990fd32ac5b1e7502a173622771f2e7f9aef2b
root <root@BZD25020.kingsoft.cn>
2020-07-28 20:24:38 +0800

    first commit

---
+++ lib/rocksdb/util/build_version.cc
@@ -0,0 +1,5 @@
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:fa9b061b8717cbecbae370762c097b41f32107aa";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2020-07-28";
+const char* rocksdb_build_compile_date = __DATE__;
